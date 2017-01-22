8c9cba2d385d2e8be2f6e7fd4ae29689f7ec62ec
Michael York <me@myork.net>
2017-01-22 11:40:18 -0500

    Fixed Drive Subsystem.

--- src/org/usfirst/frc/team3680/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team3680/robot/subsystems/DriveSubsystem.java
@@ -6,18 +6,20 @@ import org.usfirst.frc.team3680.robot.RobotMap;
 import org.usfirst.frc.team3680.robot.commands.DriveTeleop;
 
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.Spark;
 
 public class DriveSubsystem extends Subsystem {
 	
-	private Talon left1, left2, right1, right2;
+	private Spark left1, right1;
+	private Victor left2, right2;
 	private RobotDrive robotDrive;
 
 	public DriveSubsystem() {
-		left1 = new Talon(RobotMap.driveController1ID); // Front Left
-		left2 = new Talon(RobotMap.driveController2ID); // Rear Left
-		right1 = new Talon(RobotMap.driveController3ID); // Front Right
-		right2 = new Talon(RobotMap.driveController4ID); // Rear Right
+		left1 = new Spark(RobotMap.driveController1ID); // Front Left
+		left2 = new Victor(RobotMap.driveController2ID); // Rear Left
+		right1 = new Spark(RobotMap.driveController3ID); // Front Right
+		right2 = new Victor(RobotMap.driveController4ID); // Rear Right
 		left1.set(left2.get());
 		right2.set(right1.get());
 		robotDrive = new RobotDrive(left2, right1);
@@ -26,6 +28,8 @@ public class DriveSubsystem extends Subsystem {
 	
 	public void arcadeDrive(double forward, double rotation) {
 		robotDrive.arcadeDrive(forward, rotation);
+		left1.set(left2.get());
+		right2.set(right1.get());
 	}
 	
 	public void driveStraight(double speed) {
@@ -35,10 +39,12 @@ public class DriveSubsystem extends Subsystem {
 	
 	public void moveLeftSide(double speed) {
 		left2.set(speed);
+		left1.set(speed);
 	}
 	
 	public void moveRightSide(double speed) {
 		right1.set(speed);
+		right2.set(speed);
 	}
 	
 
